6bf9e509f2c2ce7a20c810918144f6f4ade42414
bismilahlf <bismilah.lf@gmail.com>
2018-10-04 18:48:56 +0200

    Add state and JSX to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Card from './Card';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cardList: []
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-       
+      <div>
+        <input type="text"/>
+        <Card/>
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cardInfo: []
+    }
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+      <ul>
+        <li>
+          <img src="" alt=""/>
+          <span></span>
+          <h2>pokemon</h2>
+          <span></span>
+       </li>
+      </ul>
+      
     )
   }
 }
