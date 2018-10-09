3d2e31a562a503ce89d3c96ecf240e02028f642c
bismilahlf <bismilah.lf@gmail.com>
2018-10-09 13:19:47 +0200

    Add to github pages

---
+++ docs/precache-manifest.0e155270af95821c1531fb8151b64748.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1b063570c6b9a75398be",
+    "url": "./static/js/main.1b063570.chunk.js"
+  },
+  {
+    "revision": "d26cd6bbba8b366c8d0c",
+    "url": "./static/js/1.d26cd6bb.chunk.js"
+  },
+  {
+    "revision": "1b063570c6b9a75398be",
+    "url": "./static/css/main.311a26a3.chunk.css"
+  },
+  {
+    "revision": "6c2112c12102d2df089aea1c7dc1c743",
+    "url": "./index.html"
+  }
+];
